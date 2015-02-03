22eb19bec954c7f1c2a9459f9bb9e62e8d660936
Jesse Harlin the Harlinator <harlinjesse@gmail.com>
2015-02-03 17:21:25 -0600

    updated the Ie shims

--- compatibility.js
+++ compatibility.js
@@ -1,5 +1,8 @@
 require('./compatibility/json3.js');
+
 require('./compatibility/es5-shim.js');
-require('./compatibility/ie-shiv.js');
 require('./compatibility/es5-sham.js');
+
+require('./compatibility/ie-shiv.js');
+
 require('./compatibility/console-shim.js');
